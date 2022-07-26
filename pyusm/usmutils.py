1c83f193e44f0109332374e3666c1727adeb0a03
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-07-26 16:46:51 -0700

    change output of ngon_coords to single 2d array
    Changed the output of ngon_coords from the tuple of (x_vals, y_vals) to a single 2d numpy array where each row contains one x val and one y val.

--- pyusm/usmutils.py
+++ pyusm/usmutils.py
@@ -4,7 +4,9 @@ Created on Thu Jul 21 15:00:00 2022
 
 @author: Wuestney
 """
-class CGR():
+import numpy as np
+
+class CGR:
     """
     Creates a container for CGR coordinates with attributes related to the alphabet of the sequence,
     coordinates of alphabet, specifying forward or backward generated coordinates and form of CGR (either USM or 2D, default is USM).
@@ -21,3 +23,30 @@ class CGR():
     def coord_dictMake(self, alphabet, vertices):
         #creates the coord_dict from the list of alphabet and vertices in correct order
         self.coord_dict=dict(zip(alphabet, vertices))
+
+def ngon_coords(verts):
+    """
+    Takes number of desired vertices and outputs the x, y coordinates of a regular
+    n-gon with that number of vertices.
+
+    Parameters
+    ----------
+    verts : INT
+        NUMBER OF DESIRED VERTICES.
+
+    Returns
+    -------
+
+    vert_array : 2D NUMPY ARRAY
+        N x 2 NUMPY ARRAY, WHERE N=verts. EACH ROW IS AN X,Y COORDINATE FOR ONE
+        THE VERTICES OF THE N-GON.s
+
+    """
+    radians=[]
+    for k in range(verts):
+        rad = (2*np.pi*k)/verts
+        radians.append(rad)
+    x_vals = np.cos(radians)
+    y_vals =np.sin(radians)
+    vert_array = np.column_stack((x_vals, y_vals))
+    return vert_array
