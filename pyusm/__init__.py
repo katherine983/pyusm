5604593de2a2179375cfcd00a4ca4925b693864c
katherine983 <53629686+katherine983@users.noreply.github.com>
2023-07-24 17:38:58 -0700

    update git ignore

--- pyusm/__init__.py
+++ pyusm/__init__.py
@@ -5,6 +5,7 @@ Created on Wed Jan 19 16:36:37 2022
 @author: Wuestney
 """
 
-from .usmutils import ngon_coords
+# from .usmutils import ngon_coords
+from .usmutils import *
 from .usm import *
 from . import usm_entropy
